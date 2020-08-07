64d1f6433eb38ad468302119df0666a285b21048
Matej Danic <matej.danic@triple-innovations.com>
2020-08-07 23:27:17 +0200

    removed role adding on startup

--- src/main/java/matej/startup/AppStartupComponent.java
+++ src/main/java/matej/startup/AppStartupComponent.java
@@ -2,20 +2,20 @@ package matej.startup;
 
 import java.util.Arrays;
 
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
+// import org.springframework.boot.ApplicationRunner;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import matej.api.repositories.RoleRepository;
-import matej.models.Role;
+// import matej.api.repositories.RoleRepository;
+// import matej.models.Role;
 
 @Component
 public class AppStartupComponent {    
-    @Bean
-    public ApplicationRunner initializer(RoleRepository roleRepo) {
-        return args -> roleRepo.saveAll(Arrays.asList(
-            new Role("USER"),
-            new Role("ADMIN")
-        ));
-    }
+    // @Bean
+    // public ApplicationRunner initializer(RoleRepository roleRepo) {
+    //     return args -> roleRepo.saveAll(Arrays.asList(
+    //         new Role("USER"),
+    //         new Role("ADMIN")
+    //     ));
+    // }
 }
