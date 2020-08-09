a3869bc624ff34b630191e2e0099194664156c5b
Matej <m.djanic@gmail.com>
2020-08-09 11:22:39 +0200

    recreate database

--- src/main/java/matej/api/controllers/HomeController.java
+++ src/main/java/matej/api/controllers/HomeController.java
@@ -1,7 +1,5 @@
 package matej.api.controllers;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import matej.api.services.ScoreService;
 import matej.constants.JambConstants;
-import matej.models.Score;
 
 
 @RestController
--- src/main/java/matej/models/Score.java
+++ src/main/java/matej/models/Score.java
@@ -67,4 +67,12 @@ public class Score {
 	public void setDate(LocalDate date) {
 		this.date = date;
 	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
