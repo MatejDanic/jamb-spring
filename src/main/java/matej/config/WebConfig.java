a0af7fd7750bf374e8bb3c69ff541d7de5baf3f5
Matej Danic <matej.danic@triple-innovations.com>
2020-08-25 15:10:45 +0200

    reset allowed cors headers

--- src/main/java/matej/config/WebConfig.java
+++ src/main/java/matej/config/WebConfig.java
@@ -23,9 +23,6 @@ public class WebConfig implements WebMvcConfigurer {
 	 */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowCredentials(true)               
-        .allowedHeaders("Authorization", "Cache-Control", "Content-Type", "Accept", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
-        .exposedHeaders("Access-Control-Expose-Headers", "Authorization", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
-        .allowedMethods("POST", "GET", "PUT", "DELETE");
+        registry.addMapping("/**").allowedMethods("POST", "GET", "PUT", "DELETE");
     }
 }
